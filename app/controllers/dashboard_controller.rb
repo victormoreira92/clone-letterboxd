36574312f364e90309a9b78000dec313b5a3cd0f
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-04-03 17:18:08 -0300

    feat: Add movie in page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
-  
+
+
+  def index
+    @movies_latest ||= Tmdb::Movie.latest
+    @movies_now_playing ||= Tmdb::Movie.latest
+    @movies_upcoming ||= Tmdb::Movie.upcoming
+    @movies_top_rated ||= Tmdb::Movie.top_rated
+  end
+
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,6 @@
 module DashboardHelper
+
+  def url_image(movie)
+    movie.poster_path.present? ? "https://image.tmdb.org/t/p/w500/#{movie.poster_path}" : 'placeholder.jpg'
+  end
 end
