56854bdc99ac5e5949377daefcb26980cb13dd4a
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-04-03 12:18:50 -0300

    fix: Remove ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,5 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
-  def current_ability
-    @current_ability ||= Ability.new(current_user)
-  end
+
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,4 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
-
-  def index
-    @movies = FilmRequest.call("Terminator 2")
-  end
+  
 end
