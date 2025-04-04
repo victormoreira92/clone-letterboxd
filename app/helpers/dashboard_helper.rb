6a92e56795359ff8e4e702da17e733fce6f457b5
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-04-04 18:48:51 -0300

    feat: Creation index page of DashboardController

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,6 @@
 module DashboardHelper
 
   def url_image(movie)
-    movie.poster_path.present? ? "https://image.tmdb.org/t/p/w500/#{movie.poster_path}" : 'placeholder.jpg'
+    movie.poster_path.present? ? "https://image.tmdb.org/t/p/w500/#{movie.poster_path}" : ''
   end
 end
